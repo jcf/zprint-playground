f28c7dfec1956336da63be148b9483ba3c6a979f
James Conroy-Finn <git@invetica.co.uk>
2023-12-16 13:40:22 +0000

    Add how-to-ns

---
+++ cases/how-to-ns/in.clj
@@ -0,0 +1,5 @@
+(ns in
+  (:import [java.io File] (java.util Date))
+  (:require (clojure.java.io :as io) [clojure.string :as str]
+      [clojure.data.csv ]
+    [clojure.pprint :refer [pprint]]))
---
+++ cases/how-to-ns/out.clj
@@ -0,0 +1,9 @@
+(ns in
+  (:require
+   [clojure.data.csv]
+   [clojure.java.io :as io]
+   [clojure.pprint :refer [pprint]]
+   [clojure.string :as str])
+  (:import
+   (java.io File)
+   (java.util Date)))
